2263adb3c4110fd2c0771fadf2ab53044572b156
cinefanLA <luisarturo1217@gmail.com>
2025-10-10 03:05:55 +0000

    Pytest

--- test_exceptions.py
+++ test_exceptions.py
@@ -11,3 +11,26 @@ def test_divide_type_error():
     with pytest.raises(TypeError):
         demo.divide("10", 2)
 
+def test_access_list_index_error():
+    with pytest.raises(IndexError):
+        demo.access_list([1, 2, 3], 5)
+
+def test_access_list_type_error():
+    with pytest.raises(TypeError):
+        demo.access_list(1, 0)
+
+def test_access_dict_key_error():
+    with pytest.raises(KeyError):
+        demo.access_dict({"a": 1, "b": 2}, "z")
+
+def test_access_dict_type_error():
+    with pytest.raises(TypeError):
+        demo.access_dict("not_a_dict", "b")
+
+def test_read_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        demo.read_file("archivo_que_no_existe.txt")
+
+def test_access_attribute_error():
+    with pytest.raises(AttributeError):
+        demo.access_attribute(object())
